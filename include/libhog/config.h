c93a37bcf004360ae98b3748007e01034b701222
Lukas Krickl <lukas@krickl.dev>
2023-07-17 06:40:45 +0200

    Added basic types

--- include/libhog/config.h
+++ include/libhog/config.h
@@ -1,7 +1,9 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-struct hog_config {};
+struct hog_config {
+  int arch_size;
+};
 
 struct hog_config hog_config_init(void);
 
--- include/libhog/types.h
+++ include/libhog/types.h
@@ -1,10 +1,36 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-enum hog_types { HOG_TYPE_U8 };
+#include "libhog/macros.h"
+
+enum hog_types {
+  HOG_TYPE_VOID,
+
+  HOG_TYPE_U8,
+  HOG_TYPE_U16,
+  HOG_TYPE_U32,
+  HOG_TYPE_U64,
+
+  // size depends on the arch_size in config
+  HOG_TYPE_USIZE,
+
+  HOG_TYPE_I8,
+  HOG_TYPE_I16,
+  HOG_TYPE_I32,
+  HOG_TYPE_I64,
+
+  // size depends on the arch_size in config
+  HOG_TYPE_ISIZE,
+
+  HOG_TYPE_STRUCT,
+  HOG_TYPE_ENUM,
+};
 
 struct hog_type {
   enum hog_types type;
+
+  // size depends on the arch_size in config
+  bool ptr;
 };
 
 #endif
