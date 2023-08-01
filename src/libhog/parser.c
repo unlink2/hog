e149889e4feb8f2f63419a593733dcafa78baa5c
Lukas Krickl <lukas@krickl.dev>
2023-08-01 18:27:19 +0200

    Added tests for parser

--- src/libhog/parser.c
+++ src/libhog/parser.c
@@ -1,3 +1,22 @@
 #include "libhog/parser.h"
+#include <ctype.h>
 
-const char *hog_tok_next(const char *input, size_t *len) { return input; }
+const char *hog_tok_next(const char *input, size_t *len) {
+  while (*input != '\0' && isspace(*input)) {
+    input++;
+  }
+
+  const char *start = input;
+
+  *len = 0;
+  if (*start == '\0') {
+    return NULL;
+  }
+
+  while (*input != '\0' && !isspace(*input)) {
+    input++;
+    *len += 1;
+  }
+
+  return start;
+}
--- src/libhog/test/parser.c
+++ src/libhog/test/parser.c
@@ -3,4 +3,14 @@
 #include <cmocka.h>
 #include <stdlib.h>
 
-void test_parser(void **state) {}
+void test_parser(void **state) {
+
+  size_t n = 0;
+  assert_memory_equal("test", hog_tok_next("test tokens", &n), 4);
+  assert_int_equal(n, 4);
+
+  assert_memory_equal("tokens", hog_tok_next("   tokens", &n), 6);
+  assert_int_equal(n, 6);
+
+  assert_null(hog_tok_next("", &n));
+}
