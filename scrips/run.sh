701cfb6c812fe3b5306d701ae4e2864e1f5a0a04
Lukas Krickl <lukas@krickl.dev>
2023-08-07 17:56:08 +0200

    Added mem parameter to run script

--- scrips/run.sh
+++ scrips/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-premake5 gmake && make &&
-	valgrind --leak-check=full --track-origins=yes ./bin/Debug/hog $@
+
+if [ "${1}" = "mem" ]; then
+  premake5 gmake && make &&
+    valgrind --leak-check=full --track-origins=yes ./bin/Debug/hog "$@"
+else   
+  premake5 gmake && make && ./bin/Debug/hog "$@"
+fi
+
+
